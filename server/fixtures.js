9ae51800979cd00aeb945321eec23c8332caa97f
yangxy <yangxy@mxchip.com>
2015-12-09 12:06:08 +0800

    path test 2

--- server/fixtures.js
+++ server/fixtures.js
@@ -14,3 +14,25 @@ if (Posts.find().count() === 0) {
     url: 'http://themeteorbook.com'
   });
 }
+if (Navs.find().count() === 0) {
+  Navs.insert({name: 'monitor', text: '监控中心'});
+  Navs.insert({name: 'solo', text: '单机监控'});
+  Navs.insert({name: 'group', text: '群组监控'});
+  Navs.insert({name: 'custom', text: '自定义监控'});
+
+  Navs.insert({name: 'repair', text: '维修状态'});
+  Navs.insert({name: 'repairlog', text: '维修记录'});
+
+  Navs.insert({name: 'service', text: '维保状态'});
+  Navs.insert({name: 'year', text: '年检状态'});
+  
+  Navs.insert({name: 'stat_broke', text: '故障统计'});
+  Navs.insert({name: 'stat_time', text: '运行时间'});
+  Navs.insert({name: 'stat_service', text: '服务统计'});
+
+  Navs.insert({name: 'msg_user', text: '用户消息'});
+  Navs.insert({name: 'msg_sys', text: '系统消息'});
+
+  Navs.insert({name: 'users', text: '账号管理'});
+  Navs.insert({name: 'devices', text: '设备管理'});
+}
--- server/publications.js
+++ server/publications.js
@@ -1,3 +1,6 @@
 Meteor.publish('posts', function() {
   return Posts.find();
 });
+Meteor.publish('navs', function() {
+  return Navs.find();
+});
