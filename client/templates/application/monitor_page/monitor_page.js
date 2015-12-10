cdfaab2b02c530ffe1ed335934c6b497f3588728
yangxy <yangxy@mxchip.com>
2015-12-10 13:27:37 +0800

    img test

--- client/templates/application/monitor_page/monitor_page.js
+++ client/templates/application/monitor_page/monitor_page.js
@@ -1,5 +1,9 @@
-// Template.content.helpers({
-//   posts: function() {
-//     return Posts.find();
-//   }
-// });
+setTimeout(function() {
+  $("#monitor_nav li").click(function(){
+    var name = $(this).attr('name');
+  	if ($('#'+name).class() != name) {
+      $("#monitor_nav li").toggleClass('active');
+      $(this).parent().parent().siblings().toggle();
+    }
+  });
+}, 500);
