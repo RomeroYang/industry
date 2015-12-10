bff7622a46366fec194dbbadae9e9f5b26e73a27
yangxy <yangxy@mxchip.com>
2015-12-10 14:32:55 +0800

    monitor page 3

--- client/templates/application/monitor_page/monitor_page.js
+++
@@ -1,9 +0,0 @@
-setTimeout(function() {
-  $("#monitor_nav li").click(function(){
-    var name = $(this).attr('name');
-  	if ($('#'+name).css('display') == 'none') {
-      $("#monitor_nav li").toggleClass('active');
-      $(this).parent().parent().siblings().toggle();
-    }
-  });
-}, 500);
--- client/templates/application/solo_page/solo_page.js
+++ client/templates/application/solo_page/solo_page.js
@@ -1,5 +1,9 @@
-// Template.content.helpers({
-//   posts: function() {
-//     return Posts.find();
-//   }
-// });
+setTimeout(function() {
+  $("#solo_nav li").click(function(){
+    var name = $(this).attr('name');
+  	if ($('#'+name).css('display') == 'none') {
+      $(this).parent().parent().siblings().toggle();
+      $("#solo_nav li").toggleClass('active');
+    }
+  });
+}, 500);
