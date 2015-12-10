ab4db59f80cea4148b51658fec892bf5c2362573
yangxy <yangxy@mxchip.com>
2015-12-10 13:36:34 +0800

    monitor page

--- client/templates/application/monitor_page/monitor_page.js
+++ client/templates/application/monitor_page/monitor_page.js
@@ -1,7 +1,7 @@
 setTimeout(function() {
   $("#monitor_nav li").click(function(){
     var name = $(this).attr('name');
-  	if ($('#'+name).class() != name) {
+  	if ($('#'+name).attr('class') != name) {
       $("#monitor_nav li").toggleClass('active');
       $(this).parent().parent().siblings().toggle();
     }
