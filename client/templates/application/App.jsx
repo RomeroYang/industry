c7315d8128611c55f570041b17037790134fc43a
yangxy <yangxy@mxchip.com>
2015-12-17 10:40:52 +0800

    react test

--- client/templates/application/App.jsx
+++ client/templates/application/App.jsx
@@ -16,22 +16,22 @@ App = React.createClass({
   render() {
     return (
       <div>
+hello
+        // <nav className="navbar navbar-inverse navbar-fixed-top">
+        //   <Header />
+        // </nav>
 
-        <nav className="navbar navbar-inverse navbar-fixed-top">
-          <Header />
-        </nav>
+        // <div className="container-fluid">
+        //   <div className="row">
+        //     <div className="col-sm-3 col-md-2 sidebar">
+        //       <LeftNav changePage={this._changePage} />
+        //     </div>
 
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-sm-3 col-md-2 sidebar">
-              <LeftNav changePage={this._changePage} />
-            </div>
-
-            <div id="main" className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-              <Content currentPage={this.state.currentPage} />
-            </div>
-          </div>
-        </div>
+        //     <div id="main" className="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+        //       <Content currentPage={this.state.currentPage} />
+        //     </div>
+        //   </div>
+        // </div>
 
       </div>
     );
