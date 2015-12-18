dad4f7e1614881208fb26a3537960a5b5dc20765
yangxy <yangxy@mxchip.com>
2015-12-18 11:27:06 +0800

    add manage pages3

--- client/templates/application/LeftNav.jsx
+++ client/templates/application/LeftNav.jsx
@@ -27,8 +27,8 @@ LeftNav = React.createClass({
       ] },
       { name: 'account', text: "账户信息", content: [] },
       { name: 'manage', text: "管理设置", content: [
-        { name: 'manage_user', text: "账号管理"},
-        { name: 'manage_device', text: "设备管理"},
+        { name: 'manage_users', text: "账号管理"},
+        { name: 'manage_devices', text: "设备管理"},
         { name: 'manage_system', text: "系统配置"}
       ] }
     ];
