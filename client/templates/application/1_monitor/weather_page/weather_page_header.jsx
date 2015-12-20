d57ecfb89416d208dd5d4a05e6bf26d7b457732c
yangxy <yangxy@mxchip.com>
2015-12-20 19:06:33 +0800

    weather header fix2

--- client/templates/application/1_monitor/weather_page/weather_page_header.jsx
+++ client/templates/application/1_monitor/weather_page/weather_page_header.jsx
@@ -5,13 +5,11 @@ WeatherPageHeader = React.createClass({
     var onlineName = this.props.currentDevice.online-0 ? "online-circle" : "offline-circle";
     return (
     	<div className="container-fluid pageheader">
-      	  <div className="row">
-      	     <div className="pull-left">{this.props.weather._id}</div>
-      	     <div className="pull-right">
-      	       <div className={onlineName}></div>
-      	       <div className="isonline">{isOnline}</div>
-      	     </div>
-      	  </div>
+  	     <div className="pull-left">{this.props.weather._id}</div>
+  	     <div className="pull-right">
+  	       <div className={onlineName}></div>
+  	       <div className="isonline">{isOnline}</div>
+  	     </div>
       </div>
     );
   }
