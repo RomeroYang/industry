4c567db6bbc5c258a4ae740e11e6a0d163223dfa
yangxy <yangxy@mxchip.com>
2015-12-20 17:15:07 +0800

    hight chart test

--- client/templates/application/1_monitor/monitor_solo_page.jsx
+++ client/templates/application/1_monitor/monitor_solo_page.jsx
@@ -38,6 +38,8 @@ MonitorSoloPage = React.createClass({
   _onMessageArrived(message) {
     //console.log("onMessageArrived:"+message.payloadString);
     var msg = JSON.parse(message.payloadString);
+    console.log(this.state.currentMessage);
+    console.log(msg);
     if (this.state.currentMessage !== msg) {
       var data_array_str = localStorage.getItem('60b3105c/d0bae40090fb');
       if (data_array_str) {
@@ -58,6 +60,55 @@ MonitorSoloPage = React.createClass({
     }
   },
 
+  _showChart() {
+    var chart_container = this.refs.chart;
+    $(chart_container).highcharts({
+        title: {
+            text: 'Monthly Average Temperature',
+            x: -20 //center
+        },
+        subtitle: {
+            text: 'Source: WorldClimate.com',
+            x: -20
+        },
+        xAxis: {
+            categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+        },
+        yAxis: {
+            title: {
+                text: 'Temperature (°C)'
+            },
+            plotLines: [{
+                value: 0,
+                width: 1,
+                color: '#808080'
+            }]
+        },
+        tooltip: {
+            valueSuffix: '°C'
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle',
+            borderWidth: 0
+        },
+        series: [{
+            name: 'Tokyo',
+            data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+        }, {
+            name: 'New York',
+            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+        }, {
+            name: 'Berlin',
+            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
+        }, {
+            name: 'London',
+            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
+        }]
+    });
+  },
+
   componentDidMount() {
     this._mqttClient();
     var client = this.mqttClient;
@@ -68,6 +119,15 @@ MonitorSoloPage = React.createClass({
         var device_id = '60b3105c/d0bae40090fb';
         client.subscribe(device_id + "/out");   //订阅消息
     };
+
+    var history_msg_str = localStorage.getItem('60b3105c/d0bae40090fb');
+    var history_msg = JSON.parse(history_msg_str);
+
+    this._showChart();
+
+    // this._chartInterval = setInterval(function() {
+    //   //
+    // }, 3000);
   },
 
   componentWillUnmount() {
@@ -86,6 +146,7 @@ MonitorSoloPage = React.createClass({
         <div>
           {JSON.stringify(this.state.historyMessages)}
         </div>
+        <div ref="chart"></div>
 		  </div>
       </div>
     );
